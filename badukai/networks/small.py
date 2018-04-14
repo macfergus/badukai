95369096c63ae15c70a69c907e284abcf180f74a
Kevin Ferguson <kevin.a.ferguson@gmail.com>
2018-04-13 19:51:27 -0700

    Add a smaller network.

---
+++ badukai/networks/small.py
@@ -0,0 +1,22 @@
+from keras.layers import Activation, BatchNormalization, Conv2D
+
+
+def conv_unit(filter_size, num_filters, input_shape=None):
+    return [
+        Conv2D(
+            num_filters, (filter_size, filter_size),
+            padding='same',
+            data_format='channels_first'),
+        BatchNormalization(axis=1),
+        Activation('relu'),
+    ]
+
+
+def layers(input_shape):
+    return \
+        conv_unit(3, 128, input_shape=input_shape) + \
+        conv_unit(3, 128) + \
+        conv_unit(3, 128) + \
+        conv_unit(3, 128) + \
+        conv_unit(3, 128) + \
+        conv_unit(3, 128)
