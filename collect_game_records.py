afc14240806dffb4911738dd6cb66ad833c9a9a2
Kevin Ferguson <kevin.a.ferguson@gmail.com>
2018-04-15 20:05:19 -0700

    Remove unusued code.

--- collect_game_records.py
+++ collect_game_records.py
@@ -1,61 +1,7 @@
 import argparse
 import boto3
-import datetime
 import os
 import tempfile
-import uuid
-
-import requests
-
-import baduk
-import badukai
-
-
-def record_game(black_bot, white_bot):
-    assert black_bot.board_size() == white_bot.board_size()
-    board_size = black_bot.board_size()
-
-    max_game_length = 2 * board_size * board_size
-
-    players = {
-        baduk.Player.black: black_bot,
-        baduk.Player.white: white_bot,
-    }
-
-    num_moves = 0
-    game = baduk.GameState.new_game(board_size)
-    builder = badukai.bots.zero.GameRecordBuilder()
-    printer = badukai.io.AsciiBoardPrinter()
-    while not game.is_over():
-        next_bot = players[game.next_player]
-        if num_moves < 20:
-            next_bot.set_temperature(1.0)
-        else:
-            next_bot.set_temperature(0.0)
-        if num_moves >= max_game_length:
-            next_move = baduk.Move.pass_turn()
-        else:
-            next_move = next_bot.select_move(game)
-            builder.record_move(
-                game.next_player,
-                next_move,
-                next_bot.search_counts())
-        num_moves += 1
-        game = game.apply_move(next_move)
-        printer.print_board(game.board)
-        print('')
-
-    game_result = badukai.scoring.compute_game_result(game)
-    printer.print_result(game_result)
-    builder.record_result(game_result.winner)
-    return builder.build()
-
-
-def get_bot_from_url(url):
-    tempfd, tempfname = tempfile.mkstemp(prefix='tmp-httpbot')
-    resp = requests.get(url)
-    os.write(tempfd, resp.content)
-    return tempfname
 
 
 def main():
