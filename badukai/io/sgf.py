771891973152c7ac71d753c6fd5a5ba94b529b46
Kevin Ferguson <kevin.a.ferguson@gmail.com>
2018-05-13 21:28:03 -0700

    SGF reader.

--- badukai/io/sgf.py
+++ badukai/io/sgf.py
@@ -1,7 +1,14 @@
-from baduk import Player
+from collections import namedtuple
+
+from parsimonious.grammar import Grammar
+from parsimonious.nodes import NodeVisitor
+
+from baduk import Board, GameState, Move, Player, Point
 
 __all__ = [
+    'GameRecord',
     'save_game_as_sgf',
+    'read_game_from_sgf',
 ]
 
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
@@ -9,6 +16,15 @@ ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 def point_to_sgf(point, num_rows):
     row_idx = num_rows - point.row
     return ALPHABET[point.col - 1] + ALPHABET[row_idx]
+
+
+def decode_sgf_move(sgf_move, num_rows):
+    if sgf_move == '':
+        return Move.pass_turn()
+    assert len(sgf_move) == 2
+    col = ALPHABET.index(sgf_move[0]) + 1
+    row = num_rows - ALPHABET.index(sgf_move[1])
+    return Move.play(Point(row, col))
 
 
 def save_game_as_sgf(outf, game, game_result,
@@ -43,3 +59,134 @@ def save_game_as_sgf(outf, game, game_result,
         elif move.is_pass:
             outf.write(';{player}[]'.format(player=player_name))
     outf.write(')')
+
+sgf_grammar = Grammar('''
+    collection = gametree+
+    gametree = "(" sequence gametree* ")" whitespace
+    sequence = node+
+    node = ";" properties
+    properties = padded_property*
+    padded_property = property whitespace
+    property = propident propvalues
+    propident = ~"[A-Z][A-Z]?"
+    propvalues = propvalue+
+    propvalue = "[" value "]" whitespace
+    value = ~"[^\]]*"
+    whitespace = ~"\s*"
+''')
+
+
+class SGFProperty(namedtuple('SGFProperty', 'ident values')):
+    pass
+
+
+class SGFVisitor(NodeVisitor):
+    """
+    Only handles SGFs with a single sequence.
+    """
+    def visit_collection(self, node, children):
+        # collection = gametree+
+        gametrees = children[0]
+        return gametrees
+
+    def visit_gametree(self, node, children):
+        # gametree = "(" sequence gametree* ")" whitespace
+        _, sequence, _, _, _ = children
+        return sequence
+
+    def visit_sequence(self, node, children):
+        # sequence = node+
+        return children
+
+    def visit_node(self, node, children):
+        _, props = children
+        return props
+
+    def visit_padded_property(self, node, children):
+        prop, _ = children
+        return prop
+
+    def visit_property(self, node, children):
+        propident, propvalues = children
+        return SGFProperty(propident, propvalues)
+
+    def visit_propident(self, node, children):
+        return node.text
+
+    def visit_propvalue(self, node, children):
+        _, value, _, _ = children
+        return value
+
+    def visit_propvalues(self, node, children):
+        return children
+
+    def visit_value(self, node, children):
+        return node.text
+
+    def visit_whitespace(self, node, whitespace):
+        pass
+
+    def generic_visit(self, node, children):
+        return children
+
+
+class GameRecord(namedtuple('GameRecord', 'initial_state moves winner')):
+    pass
+
+
+NO_DEFAULT = object()
+def get_prop(proplist, ident, default=NO_DEFAULT):
+    for prop in proplist:
+        if prop.ident == ident:
+            assert len(prop.values) == 1
+            return prop.values[0]
+    if default is NO_DEFAULT:
+        raise KeyError(ident)
+    return default
+
+
+def get_props(proplist, ident):
+    for prop in proplist:
+        if prop.ident == ident:
+            return prop.values
+    raise KeyError(ident)
+
+
+def read_game_from_sgf(sgffile):
+    tree = sgf_grammar.parse(sgffile.read())
+    visitor = SGFVisitor()
+    sequence = visitor.visit(tree)
+    root, moves = sequence[0], sequence[1:]
+    board_size = int(get_prop(root, 'SZ'))
+    komi = float(get_prop(root, 'KM'))
+    result = get_prop(root, 'RE')
+    if result.startswith('B+'):
+        winner = Player.black
+    elif result.startswith('W+'):
+        winner = Player.white
+    else:
+        raise ValueError(result)
+    handicap = int(get_prop(root, 'HA', 0))
+    board = Board(board_size, board_size)
+    first_player = Player.black
+    if handicap > 0:
+        handicap_stones = get_props(root, 'AB')
+        for sgf_move in handicap_stones:
+            move = decode_sgf_move(sgf_move, board_size)
+            board.place_stone(Player.black, move.point)
+            first_player = Player.white
+    initial_state = GameState.from_board(board, first_player, komi)
+    state = initial_state
+    decoded_moves = []
+    for node in moves:
+        key = 'B' if state.next_player == Player.black else 'W'
+        sgf_move = get_prop(node, key)
+        move = decode_sgf_move(sgf_move, board_size)
+        decoded_moves.append(move)
+        state = state.apply_move(move)
+
+    return GameRecord(
+        initial_state=initial_state,
+        moves=decoded_moves,
+        winner=winner
+    )
