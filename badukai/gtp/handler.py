f95cfe6a222959b23f19142540be9611c433ad96
Kevin Ferguson <kevin.a.ferguson@gmail.com>
2018-05-25 07:07:33 -0700

    Handle boardsize command.

--- badukai/gtp/handler.py
+++ badukai/gtp/handler.py
@@ -9,7 +9,15 @@ class BotHandler:
     def __init__(self, bot):
         self.is_done = False
         self.bot = bot
+        self.board_size = self.bot.board_size()
 
     def handle_quit(self):
         self.is_done = True
         return success('bye!')
+
+    def handle_boardsize(self, board_size):
+        board_size = int(board_size)
+        if board_size != self.board_size:
+            return failure('only support {}x{}'.format(
+                self.board_size, self.board_size))
+        return success('{}'.format(board_size))
