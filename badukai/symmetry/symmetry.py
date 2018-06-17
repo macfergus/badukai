e3338ff6fd5fb5b17b068231e421c39df51f2d2a
Kevin Ferguson <kevin.a.ferguson@gmail.com>
2018-06-17 14:12:23 -0700

    Add rotate_board function.

--- badukai/symmetry/symmetry.py
+++ badukai/symmetry/symmetry.py
@@ -2,6 +2,7 @@ import baduk
 
 __all__ = [
     'rotate_point',
+    'rotate_board',
 ]
 
 
@@ -17,3 +18,17 @@ def rotate_point(p, index, board_size):
         # Swap rows & cols
         p = baduk.Point(row=p.col, col=p.row)
     return p
+
+
+def rotate_board(board, index):
+    size = board.num_rows
+    assert board.num_cols == size
+    new_board = baduk.Board(size, size)
+    for r in range(1, size + 1):
+        for c in range(1, size + 1):
+            p = baduk.Point(row=r, col=c)
+            stone = board.get(p)
+            if stone is not None:
+                new_point = rotate_point(p, index, size)
+                new_board.place_stone(stone, new_point)
+    return new_board
--- badukai/symmetry/symmetry_test.py
+++ badukai/symmetry/symmetry_test.py
@@ -21,3 +21,14 @@ class SymmetryTest(unittest.TestCase):
 
         rotated = [symmetry.rotate_point(orig, i, 19) for i in range(8)]
         self.assertCountEqual(rotated, rotations)
+
+    def test_rotate_board(self):
+        board = baduk.Board(5, 5)
+        board.place_stone(baduk.Player.black, baduk.Point(3, 3))
+        board.place_stone(baduk.Player.white, baduk.Point(3, 2))
+        board.place_stone(baduk.Player.black, baduk.Point(4, 2))
+
+        rotated = symmetry.rotate_board(board, 1)
+        self.assertEqual(baduk.Player.black, rotated.get(baduk.Point(3, 3)))
+        self.assertEqual(baduk.Player.white, rotated.get(baduk.Point(3, 2)))
+        self.assertEqual(baduk.Player.black, rotated.get(baduk.Point(2, 2)))
